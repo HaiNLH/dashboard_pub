be645b70510ed743dc3315945f7ad6d7a4340db0
hainlh <longhai230303@gmail.com>
2023-12-18 18:19:30 +0700

    adding chart

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from dash import Dash, html, dcc, Input, Output, dash_table
 import dash_bootstrap_components as dbc
 from dash import dcc
 import plotly.graph_objs as go
+import plotly.express as px
 import pandas as pd
 import numpy as np
 import requests
@@ -31,6 +32,7 @@ app.layout = html.Div(children = [
                 "flex-direction": "column",
                 "align-items": "center",
                 "justify-content": "center",}),
+    
     html.Div(children = [
         dash_table.DataTable(id = "ft_open_table",page_size=20),
         ],
@@ -44,9 +46,16 @@ app.layout = html.Div(children = [
         dropdown_options("Quantity Precision", 
             options = ['0','1','2','3','4','5','6'], defauilt_value = 4, _id = 'quantity_precision'),
         dropdown_options("Size", 
-        options = ['100','500','1000','5000','10000'], defauilt_value = 5000, _id = 'size'),
+        options = ['100','500','1000','5000','8000','10000'], defauilt_value = 500, _id = 'size'),
 
     ], style = {'padding-left' : '50px'}),
+    html.Div(children = [
+        dcc.Graph(id="live-graph")
+    ],),
+    html.Div(children = [
+        dcc.Graph(id="live-graph-1")
+    ],
+     ), 
     
 
     dcc.Interval(id = "timer", interval = 3000),
@@ -95,6 +104,8 @@ def aggregate_levels(levels_df, agg_level = Decimal('0.1'), side = 'buy'):
 
 @app.callback(Output("ft_close_table", "data"),
                 Output("ft_open_table", "data"),
+                Output("live-graph", "figure"),
+                Output("live-graph-1", "figure"),
                 # Output("open-buy-graph", "figure"),
                 # Output("buy_table", "style_data_conditional"),
                 # Output("sell_table", "data"),
@@ -109,7 +120,9 @@ def aggregate_levels(levels_df, agg_level = Decimal('0.1'), side = 'buy'):
                 # Input("crypto_pair", "value"),
                 Input("quantity_precision", "value"),
                 Input("size", "value"),
-                Input("timer", "n_intervals"))
+                Input("timer", "n_intervals"),
+                )
+                
 
 # def update_ordebook(quantity_precision, price_precision, agg_level, symbol, n_intervals):
 def update_ordebook(precision,size, n_intervals):
@@ -153,8 +166,44 @@ def update_ordebook(precision,size, n_intervals):
     df2['stop_loss_give'] = df2['marginAmount'] * df2['stop_loss_pct']
     final_open = df2.groupby(['bidderAddr', 'direction']).agg({'orderId': 'count','marginAmount': 'sum', 'volume': 'sum', 'take_profit_take':'sum','stop_loss_give':'sum','fee':'sum'}).reset_index()
     format_open = ['marginAmount', 'volume', 'take_profit_take', 'stop_loss_give', 'fee']
+    buy_totals=final_open[final_open['direction'] == 'Buy'].sum()
+    sell_totals = final_open[final_open['direction'] == 'Sell'].sum()
     final_open[format_open] = final_open[format_open].applymap(lambda x: f"{x:.{precision}f}")
-    # print(final_open)
+    buy_data = final_open[final_open['direction'] == 'Buy']
+    sell_data = final_open[final_open['direction'] == 'Sell']
+    
+
+    bar_trace_margin = go.Bar(
+    x=['Buy', 'Sell'],  # X-axis values for Buy and Sell orders
+    y=[buy_totals['marginAmount'],sell_totals['marginAmount']],
+    name='Margin Amount',
+    marker=dict(color='blue')
+    )
+    bar_trace_take_profit = go.Bar(
+        x=['Buy', 'Sell'],
+        y=[buy_totals['take_profit_take'], sell_totals['take_profit_take']],
+        name='Take Profit Take',
+        marker=dict(color='green')
+    )
+
+    bar_trace_stop_loss = go.Bar(
+        x=['Buy', 'Sell'],
+        y=[buy_totals['stop_loss_give'], sell_totals['stop_loss_give']],
+        name='Stop Loss Give',
+        marker=dict(color='red')
+    )
+
+    # Create the bar chart figure
+    fig1 = go.Figure(data=[bar_trace_margin, bar_trace_take_profit, bar_trace_stop_loss], layout={
+        
+        'width': 500,
+        'title': 'Opening Tracking',
+        'barmode': 'group',
+        'xaxis': {'title': 'Order'},
+        'yaxis': {'title': 'Amount ($)'},
+        'legend': {'title': 'Category'}
+    })
+
 
     close_df = pd.DataFrame(data[data['status'] !='Open'])
     df1 = close_df
@@ -171,12 +220,39 @@ def update_ordebook(precision,size, n_intervals):
     df1['loseAmount'] =df1.apply(lambda x:(x['closePct']*x['marginAmount']) if x['closePct'] <0 else 0, axis = 1)
     df1['winAmount'] = df1.apply(lambda x: (x['closePct']*x['marginAmount']) if x['closePct'] >=0 else 0, axis = 1)
     final_close = df1.groupby(['bidderAddr', 'direction']).agg({'orderId': 'count','marginAmount': 'sum', 'volume': 'sum', 'loseAmount':'sum','winAmount':'sum','fee':'sum','fundingPayment':'sum'}).reset_index()
+    close_win_total = final_close['winAmount'].sum()
+    close_lose_total = final_close['loseAmount'].sum()
+
     format = ['marginAmount', 'volume', 'loseAmount', 'winAmount', 'fee', 'fundingPayment']
     final_close[format] = final_close[format].applymap(lambda x: f"{x:.{precision}f}")
     
+    bar_win = go.Bar(
+        x=['USERS'],  # X-axis values for Buy and Sell orders
+        y=[close_win_total],
+        name='WinAmount',
+        marker=dict(color='green')
+    )
+    bar_lost = go.Bar(
+        x=['USERS'],
+        y=[close_lose_total],
+        name='Lose Amount',
+        marker=dict(color='red')
+    )
+
+
+    # Create the bar chart figure
+    fig2 = go.Figure(data=[bar_win, bar_lost], layout={
+        
+        'width': 500,
+        'title': 'Closed Tracking',
+        'barmode': 'group',
+        'xaxis': {'title': 'Users'},
+        'yaxis': {'title': 'Amount ($)'},
+        'legend': {'title': 'Category'}
+    })
     
     
-    return (final_close.to_dict("records"), final_open.to_dict("records"),
+    return (final_close.to_dict("records"), final_open.to_dict("records"),fig1,fig2,
             # {'data': [graph],'layout' : go.Layout(title='Open Order',yaxis = dict(range=[0, max(buy_offer, sell_offer)]))},'#_buy_open_orders: ' + str(number_of_buy_open_orders),'#_sell_open_orders: ' + str(number_of_sell_open_orders)
     )
     pass
